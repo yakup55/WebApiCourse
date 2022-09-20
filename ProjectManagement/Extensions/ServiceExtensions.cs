b27d937947861f8db523aee39dab6b0fe48db847
yakup55 <yakup.0950@gmail.com>
2022-09-20 11:24:22 +0300

    First commit

---
+++ ProjectManagement/Extensions/ServiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectManagement.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(x=>x.AddPolicy("CorsPolicy",y=>y.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+        }
+    }
+}
