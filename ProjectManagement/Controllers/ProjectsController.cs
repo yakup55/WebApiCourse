7460b83b34f568f436385ff8922d07cc8df1652c
yakup55 <yakup.0950@gmail.com>
2022-09-20 11:56:37 +0300

    LoggerService

---
+++ ProjectManagement/Controllers/ProjectsController.cs
@@ -0,0 +1,40 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private List<Project> projectsList;
+        private ILogger<ProjectsController> logger;
+
+        public ProjectsController(ILogger<ProjectsController> logger)
+        {
+            this.logger = logger;
+            projectsList = new List<Project>
+            {
+                new Project{Id=Guid.NewGuid(),Name="Project 1"},
+                new Project{Id=Guid.NewGuid(),Name="Project 2"},
+                new Project{Id=Guid.NewGuid(),Name="Project 3"}
+            };
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                logger.LogInformation("Projects.Get() has been run");
+                return Ok(projectsList);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Projects.Get() has been crashed"+ ex.Message);
+                throw;
+            }
+           
+        }
+    }
+}
